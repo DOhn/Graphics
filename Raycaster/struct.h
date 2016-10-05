54256cf01c0c3ede29dc5bdb0dba45d56db86448
Daniel Ohn <dao48@nau.edu>
2016-10-04 23:57:40 -0700

    Have mercy on my soul.

--- Raycaster/struct.h
+++ Raycaster/struct.h
@@ -10,21 +10,21 @@
 #include "3dmath.h"
 
 
-typedef struct {
+struct Object{
   char *kind;
   double color[3];
-  
   union {
+  // Camera structure.  Keeps track of width and height
     struct {
       double width;
       double height;
     } camera;
-
+    // Sphere structure.  Keeps track of the center/position and radius
     struct {
       double center[3];
       double radius;
     } sphere;
-
+    // Plane structure.  Keeps track of normal vector and position vector
     struct {
       double position[3];
       double normal[3];
@@ -32,10 +32,19 @@ typedef struct {
   };
 } Object;
 
+// Objects structure of the values used in the parser.
 typedef struct objValues {
-	Object *objValue;
+	struct Object *objValue;
 	size_t objPos;
 } objValues;
 
-//struct objValues read_scene(FILE* json);
+// Structure for images.  Used with color and ppm
+typedef struct Img {
+	double width;
+	double height;
+	char* color;
+} Img;
+
+struct objValues read_scene(FILE* json);
+void ppmMaker(Img *image, FILE* fput, int num);
 //#endif
