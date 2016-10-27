0435e63ca7eede2ba24ba47188bce7d35dc6e1ee
Daniel Ohn <dao48@nau.edu>
2016-10-27 16:01:26 -0700

    Fix math and pixel shader.

--- Raycaster/raycaster.c
+++ Raycaster/raycaster.c
@@ -1,12 +1,10 @@
 #include "struct.h"
-// Plymorphism in C
+// Ploymorphism in C
 
 void shadePixel(double *Col, Img *image, int i, int j) {
-  image->color[(int)(i * image->width*4 + j*4+0)] = (char)Col[0];
-  image->color[(int)(i * image->width*4 + j*4+1)] = (char)Col[1];
-  image->color[(int)(i * image->width*4 + j*4+2)] = (char)Col[2];
-  image->color[(int)(i * image->width*4 + j*4+3)] = 255;
-
+  image->color[(int)(j * 3 + (image->height - i - 1) * image->width*3)] = (unsigned char)(Col[0]*255);
+  image->color[(int)(j * 3 + (image->height - i - 1) * image->width*3+1)] = (unsigned char)(Col[1]*255);
+  image->color[(int)(j * 3 + (image->height - i - 1) * image->width*3+2)] = (unsigned char)(Col[2]*255);
 }
 
 static inline double sqr(double v) {
@@ -20,77 +18,38 @@ static inline void normalize(double* v) {
   v[2] /= len;
 }
 
-double sphere_intersection(double *Ro, double *Rd, double *Cent, double R) {
-  double num_a, num_b, num_c, sol_a, sol_b;
-
-  num_a = sqr(Rd[0]) + sqr(Rd[1]) + sqr(Rd[2]);
-  num_b = (2*Ro[0]*Rd[0]-Rd[0]*Cent[0]) + (Ro[1]*Rd[1]-Rd[1]*Cent[1]) + (Ro[2]*Rd[2]-Rd[2]*Cent[2]);
-  num_c = sqr(Ro[0]-Cent[0]) + sqr(Ro[1]-Cent[1]) + sqr(Ro[2]-Cent[2]) - sqr(R);
-
-  double dist = sqr(num_b) - 4*num_a*num_c;
-
-  if (dist < 0) return -1;
-
-  dist = sqrt(dist);
-
-  double t = (-num_b, - dist) / (2 * num_a);
-  if (t > 0) return t;
-
-  t = (-num_b + dist) / (2*num_a);
-  if (t > 0) return t;
-
+double sphere_intersection(double *Ro, double *Rd, double *cent, double r) {
+  double valueA = (sqr(Rd[0]) + sqr(Rd[1]) + sqr(Rd[2]));
+  double valueB = 2 * Rd[0] * (Ro[0] - cent[0]) + 2 * Rd[1] * (Ro[1] - cent[1]) + 2 * Rd[2] * (Ro[2] - cent[2]);
+  double valueC = sqr(cent[0]) + sqr(cent[1]) + sqr(cent[2]) + sqr(Ro[0]) + sqr(Ro[1]) + sqr(Ro[2]) -2 * (cent[0] * Ro[0] + cent[1] * Ro[1] + cent[2] * Ro[2]) - sqr(r);
+  
+  double d = sqr(valueB) - 4 * valueA * valueC;
+  if(d < 0)
+    return -1;
+  
+  double t0 = (-valueB - sqrt(d)) / (2*valueA);
+  if (t0 > 0)
+    return t0;
+  
+  double t1 = (-valueB + sqrt(d)) / (2*valueA);
+  if (t1 > 0)
+    return t1;
+  
   return -1;
-
-  // if (dist < 0) {
-  //   return -1;  
-  // } else if (dist == 0) {
-  //   sol_a = -1 * (num_b / (2*num_a));
-  //   sol_b = -1 * (num_b / (2 * num_a));
-  // } else {
-  //   sol_a = (-1*num_b - sqrt(sqrt(num_b) - 4 * num_c))/2;
-  //   sol_b = (-1*num_b + sqrt(sqrt(num_b) - 4 * num_c))/2;
-  // }
-
-  // if (sol_a < 0 && sol_b < 0) {
-  //   return 0;
-  // } else if (sol_a < 0 && sol_b > 0) {
-  //   return sol_a;
-  // } else if (sol_a > 0 && sol_b < 0) {
-  //   return sol_a;
-  // } else {
-  //   if (sol_a <= sol_b) {
-  //     return sol_a;
-  //   } else {
-  //     return sol_b;
-  //   }
-  // }
 }
 
-double plane_intersection(double *Ro, double *Rd, double *Norm, double *Pos) {
-  // Declaring variables and normalizing Norm argument
-  double temp_d;
-  normalize(Norm);
-  double temp_a = v3_dot(Norm, Rd);
-  V3 a_vector;
-
-  // Subtracting vectors Pos and Ro. a_vector is set to the vaule of the resulting vector.
-  v3_sub(Pos, Ro, a_vector);
-
-  if(fabs(temp_a) < .0001) {
-    return -1;
-  }
-
-  temp_d = v3_dot(a_vector, Norm);
-  double t = -(temp_d/temp_a);
-  double colA = 0;
-
-  // If value of t is below 0 return 0.
-  if (t < 0.0) {
-    return 0;
-  }
-
-  // Else return value of t
-  return t;
+double plane_intersection(double *Ro, double *Rd, double *norm, double *pos) {
+  V3 v = malloc(sizeof(double) * 3);
+  v3_sub(Ro, pos, v);
+  
+  double dist = v3_dot(norm, v);
+  double denom = v3_dot(norm, Rd);
+  
+  dist = -(dist / denom);
+  if (dist > 0)
+    return dist;
+  
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -144,7 +103,6 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < numObjs; i++) {
     // if there is a camera objects, set the width and height of the camera
     if (objects.objValue[i].camera.width && objects.objValue[i].camera.height) {
-      printf("There is a camera\n");
       camW = objects.objValue[i].camera.width;
       camH = objects.objValue[i].camera.height;
     }
@@ -164,13 +122,12 @@ int main(int argc, char *argv[]) {
   Img *image = (Img *)malloc(sizeof(Img));
   image->height = N;
   image->width = M;
-  image->color = (char*)malloc(sizeof(char) * image->height * image->width * 4);
+  image->color = (char*)malloc(sizeof(char) * image->height * image->width * 3);
 
 
   // loop through the pixels
   for (y = 0; y < M; y++) {
     for (x = 0; x < N; x++) {
-      printf("inside for\n");
       double Ro[3] = {0, 0, 0};
 
       double Rd[3] = { objects.objValue[i].sphere.center[0] - (camW/2) + pixwidth * (x + 0.5), objects.objValue[i].sphere.center[1] - (camH/2) + pixheight * (y + 0.5), 1 };
@@ -180,18 +137,15 @@ int main(int argc, char *argv[]) {
       double best_t = INFINITY;
       // loop through the objects figuring out which one to trace
       for (i=0; i < numObjs; i++) {
-        printf("hello\n");
         double t = 0;
 
         // if the current objects is a sphere then call intersect
         if (objects.objValue[i].sphere.center && objects.objValue[i].sphere.radius) {
           t = sphere_intersection(Ro, Rd, objects.objValue[i].sphere.center, objects.objValue[i].sphere.radius);
-          printf("T\n");
         // if the current objects is a plane then call intersect
         } else if (objects.objValue[i].plane.position && objects.objValue[i].plane.normal) {
-          t = plane_intersection(Ro, Rd, objects.objValue[i].plane.position, objects.objValue[i].plane.normal);
+          t = plane_intersection(Ro, Rd, objects.objValue[i].plane.normal, objects.objValue[i].plane.position);
         }
-        //printf("This is T: %lf\n", t);
         if (t > 0 && t < best_t) {
           best_t = t;
           colHelp = i;
@@ -199,21 +153,18 @@ int main(int argc, char *argv[]) {
       }
       if (best_t > 0 && best_t != INFINITY) {
         if (objects.objValue[colHelp].sphere.center && objects.objValue[colHelp].sphere.radius) {
+          //printf("Sphere: %lf %lf %lf\n", objects.objValue[colHelp].color[0], objects.objValue[colHelp].color[1], objects.objValue[colHelp].color[2]);
           shadePixel(objects.objValue[colHelp].color, image, y, x);
-          printf("Shaded Sphere\n");
-        } else if (objects.objValue[i].plane.position && objects.objValue[i].plane.normal) {
+        } else if (objects.objValue[colHelp].plane.position && objects.objValue[colHelp].plane.normal) {
           shadePixel(objects.objValue[colHelp].color, image, y, x);
-          printf("Shaded plane\n");
         }
-        printf("#");
-      } else {
-        printf(".");
       }
     }
-    printf("\n");
   };
 
-  FILE* output = fopen(argv[4], "W+");
+  FILE* output = fopen(argv[4], "w+");
   ppmMaker(image, output, mNum);
+
+  fclose(output);
   return 0;
 }
